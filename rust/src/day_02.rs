764511ab10620dad0ea0c9e3b7f4286fbae9c344
Vytautas Strimaitis <vstrimaitis@gmail.com>
2021-12-02 08:27:00 +0200

    Add rust solution

--- rust/src/day_02.rs
+++ rust/src/day_02.rs
@@ -1,7 +1,30 @@
 use crate::common::*;
 
 pub fn solve(input: &String) -> (Option<String>, Option<String>) {
-    (None, None)
+    let commands: Vec<(&str, i32)> = get_nonempty_lines(input)
+        .map(|l| l.split_once(' ').expect("Failed to split line into parts"))
+        .map(|(x, y)| (x, y.parse::<i32>().expect("Failed to parse amount")))
+        .collect();
+    let (x1, y1) = commands
+        .iter()
+        .fold((0, 0), |(x, y), &(op, delta)| if op == "down" {
+            (x, y+delta)
+        } else if op == "up" {
+            (x, y-delta)
+        } else {
+            (x+delta, y)
+        });
+    let (x2, y2, _) = commands
+        .iter()
+        .fold((0, 0, 0), |(x, y, aim), &(op, delta)| if op == "down" {
+            (x, y, aim+delta)
+        } else if op == "up" {
+            (x, y, aim-delta)
+        } else {
+            (x+delta, y+aim*delta, aim)
+        });
+    
+    (Some((x1*y1).to_string()), Some((x2*y2).to_string()))
 }
 
 #[cfg(test)]
@@ -9,9 +32,15 @@ mod tests {
     use super::*;
     #[test]
     fn sample() {
-        let data = "TODO".to_string();
+        let data = "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+".to_string();
         let (p1, p2) = solve(&data);
-        assert_eq!(p1.as_deref(), Some("TODO").as_deref());
-        assert_eq!(p2.as_deref(), Some("TODO").as_deref());
+        assert_eq!(p1.as_deref(), Some("150").as_deref());
+        assert_eq!(p2.as_deref(), Some("900").as_deref());
     }
 }
