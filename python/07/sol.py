8fa89406a7697c9012e0c01755d178790089c794
Vytautas Strimaitis <vstrimaitis@gmail.com>
2021-12-07 07:25:05 +0200

    Add Python solution for day 7

---
+++ python/07/sol.py
@@ -0,0 +1,15 @@
+from puzzle import PuzzleContext
+
+
+def calc_fuel_usage(positions, target_position, cost_fn):
+    distances = [abs(x-target_position) for x in positions]
+    return sum(cost_fn(d) for d in distances)
+
+with PuzzleContext(year=2021, day=7) as ctx:
+    arr = [int(x) for x in ctx.data.split(",")]
+    possible_targets = list(range(min(arr), max(arr)+1))
+    ans1 = min(calc_fuel_usage(arr, p, lambda d: d) for p in possible_targets)
+    ans2 = min(calc_fuel_usage(arr, p, lambda d: d*(d+1)//2) for p in possible_targets)
+
+    ctx.submit(1, ans1)
+    ctx.submit(2, ans2)
