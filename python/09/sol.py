825a86c7b65136ae1682859f3c948b49a3070125
Vytautas Strimaitis <vstrimaitis@gmail.com>
2021-12-09 07:19:30 +0200

    Add Python solution for day 9

---
+++ python/09/sol.py
@@ -0,0 +1,38 @@
+from functools import reduce
+from puzzle import PuzzleContext
+
+def neighs(board, i, j):
+    for di, dj in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+        ii = i + di
+        jj = j + dj
+        if 0 <= ii < len(board) and 0 <= jj < len(board[0]):
+            yield ii, jj
+
+def dfs(board, i, j, visited):
+    if board[i][j] == 9 or visited[i][j]:
+        return 0
+    ans = 1
+    visited[i][j] = True
+    for ii, jj in neighs(board, i, j):
+        ans += dfs(board, ii, jj, visited)
+    return ans
+
+with PuzzleContext(year=2021, day=9) as ctx:
+    board = [[int(x) for x in r] for r in ctx.nonempty_lines]
+    n, m = len(board), len(board[0])
+    low_points = [
+        (i, j)
+        for i in range(len(board))
+        for j in range(len(board[0]))
+        if all(
+            board[i][j] < board[ii][jj]
+            for ii, jj in neighs(board, i, j)
+        )
+    ]
+    ctx.submit(1, sum(board[i][j]+1 for i, j in low_points))
+
+    sizes = []
+    visited = [[False for _ in range(m)] for _ in range(n)]
+    sizes = [dfs(board, i, j, visited) for i, j in low_points]
+    largest_sizes = list(sorted(sizes))[-3:]
+    ctx.submit(2, reduce(lambda a, b: a*b, largest_sizes))
