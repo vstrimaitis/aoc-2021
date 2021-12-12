e28d544817e201bfd448482f0bac7fe7ebae1ba3
Vytautas Strimaitis <vstrimaitis@gmail.com>
2021-12-12 18:32:20 +0200

    Print the full Rust report as a pretty table

--- rust/src/reporting.rs
+++ rust/src/reporting.rs
@@ -1,4 +1,6 @@
 use std::time::{Duration, Instant};
+use prettytable::{Table, Row, Cell, Attr, color};
+use prettytable::format::{Alignment, consts};
 
 #[derive(Debug)]
 pub struct ReportEntry {
@@ -33,28 +35,70 @@ impl Report {
         self.entries.push(entry);
     }
 
-    pub fn display(&self) {
+    pub fn display(&mut self) {
+        self.entries.sort_by(|a, b| a.day.cmp(&b.day));
+        let mut table = Table::new();
+        table.set_format(*consts::FORMAT_BORDERS_ONLY);
+        // table.set_format(*consts::FORMAT_BOX_CHARS);
 
-        println!(
-            "{0: <4} {1: <20} {2: <20} {3: >15} {4: >15} {5: >15}",
-            "Day", "Part 1", "Part 2", "Read (ms)", "Solve (ms)", "Total (ms)"
-        );
+        table.set_titles(Row::new(vec![
+            Cell::new("Day").with_style(Attr::Bold),
+            Cell::new("Part 1").with_style(Attr::Bold),
+            Cell::new("Part 2").with_style(Attr::Bold),
+            Cell::new("Read (ms)").with_style(Attr::Bold),
+            Cell::new("Solve (ms)").with_style(Attr::Bold),
+            Cell::new("Total (ms)").with_style(Attr::Bold),
+        ]));
 
         for e in self.entries.iter() {
-            println!(
-                "{0: <4} {1: <20} {2: <20} {3: >15.3} {4: >15.3} {5: >15.3}",
-                e.day,
-                e.answer_1.as_ref().unwrap_or(&"-".to_string()),
-                e.answer_2.as_ref().unwrap_or(&"-".to_string()),
-                e.read_duration.as_micros() as f32 / 1000 as f32,
-                e.solve_duration.as_micros() as f32 / 1000 as f32,
-                e.full_duration.as_micros() as f32 / 1000 as f32,
-            );
+            table.add_row(Row::new(vec![
+                Cell::new(&format!("{:02}", e.day)),
+                self.format_answer(&e.answer_1),
+                self.format_answer(&e.answer_2),
+                self.format_duration(e.read_duration, None),
+                self.format_duration(e.solve_duration, None),
+                self.format_duration(e.full_duration, None).with_style(Attr::Bold),
+            ]));
         }
         match self.end_instant {
-            Some(t) => println!("Full duration: {:.3} ms", (t - self.start_instant).as_micros() as f32 / 1000 as f32),
+            Some(t) => self.add_footer(&mut table, t - self.start_instant),
             None => ()
         };
+        table.printstd();
+    }
+
+    fn add_footer(&self, table: &mut Table, duration: Duration) {
+        table.add_row(Row::new(vec![
+            Cell::new_align("Full duration", Alignment::RIGHT).with_hspan(5).with_style(Attr::Bold),
+            self.format_duration(duration, Some(|d|
+                if d <= Duration::from_millis(100) { color::BRIGHT_GREEN }
+                else { color::BRIGHT_RED }
+            )).with_style(Attr::Bold)
+        ]));
+    }
+
+    fn format_answer(&self, ans: &Option<String>) -> Cell {
+        let empty_value = "-".to_string();
+        let value = ans.as_ref().unwrap_or(&empty_value);
+        Cell::new(&value)
+    }
+
+    fn format_duration(&self, d: Duration, color_fn: Option<fn(Duration) -> color::Color>) -> Cell {
+        let seconds = d.as_secs_f64();
+        let cell = Cell::new(&format!("{:.3}", seconds * 1000 as f64));
+        let color = match color_fn {
+            Some(pick_color) => pick_color(d),
+            _ => if d <= Duration::from_millis(2) {
+                color::BRIGHT_GREEN
+            }
+            else if d <= Duration::from_millis(4) {
+                color::BRIGHT_YELLOW
+            }
+            else {
+                color::BRIGHT_RED
+            }
+        };
+        cell.with_style(Attr::ForegroundColor(color))
     }
 }
 
