c74b7b68299fc5de10fda8fe8f0c046e12c907d5
Vytautas Strimaitis <vstrimaitis@gmail.com>
2021-12-13 14:43:45 +0200

    Refactor Python solution for day 13

--- python/13/sol.py
+++ python/13/sol.py
@@ -1,46 +1,56 @@
+from typing import List, Tuple
+from functools import reduce
 from puzzle import PuzzleContext
 
+Point = Tuple[int, int]
+Fold = Tuple[str, int]
 
-def do_fold(pts, axis, coord):
-    if axis == "y":
-        new_pts = []
-        for x, y in pts:
-            if y < coord:
-                new_pts.append((x, y))
-            else:
-                new_pts.append((x, coord-(y-coord)))
-        return new_pts
+def flip(p: Point) -> Point:
+    x, y = p
+    return y, x
+
+def do_fold(pts: List[Point], fold: Fold) -> List[Point]:
+    axis, coord = fold
+    if axis == "x":
+        flipped_pts = list(map(flip, pts))
+        return list(map(flip, do_fold(flipped_pts, ("y", coord))))
+    assert axis == "y"
     new_pts = []
     for x, y in pts:
-        if x < coord:
+        if y < coord:
             new_pts.append((x, y))
         else:
-            new_pts.append((coord-(x-coord), y))
+            new_pts.append((x, coord-(y-coord)))
     return new_pts
 
-def to_string(pts):
+def to_string(pts: List[Point]) -> str:
     xs = [x for x, _ in pts]
     ys = [y for _, y in pts]
     lines = []
     for i in range(min(ys), max(ys)+1):
-        row = []
-        for j in range(min(xs), max(xs)+1):
-            if (j, i) in pts:
-                row.append("#")
-            else:
-                row.append(" ")
+        draw_point = lambda j: "#" if (j, i) in pts else " "
+        row = map(draw_point, range(min(xs), max(xs)+1))
         lines.append("".join(row))
     return "\n".join(lines)
 
+def parse_point(s: str) -> Point:
+    parts = s.split(",")
+    return int(parts[0]), int(parts[1])
+
+def parse_split(s: str) -> Fold:
+    prefix = "fold along "
+    parts = s[len(prefix):].split("=")
+    return parts[0], int(parts[1])
+
 with PuzzleContext(year=2021, day=13) as ctx:
     pts, folds = ctx.data.split("\n\n")
-    pts = [(int(x.split(",")[0]), int(x.split(",")[1]) )for x in pts.split("\n")]
-    folds = [x[11:].split("=") for x in folds.split("\n")]
+    pts = [parse_point(x) for x in pts.split("\n")]
+    folds = [parse_split(x) for x in folds.split("\n")]
 
-    pts = do_fold(pts, folds[0][0], int(folds[0][1]))
-    ctx.submit(1, len(list(set(pts))))
+    pts_after_first_fold = do_fold(pts, folds[0])
+    unique_pts = list(set(pts_after_first_fold))
+    ctx.submit(1, len(unique_pts))
 
-    for i in range(1, len(folds)):
-        pts = do_fold(pts, folds[i][0], int(folds[i][1]))
+    final_pts = reduce(lambda p, f: do_fold(p, f), folds, pts)
     print("Part 2:")
-    print(to_string(pts))
+    print(to_string(final_pts))
