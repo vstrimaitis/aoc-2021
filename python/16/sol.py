bc190ee51f136122d7878bdd3db03868b1c4fda6
Vytautas Strimaitis <vstrimaitis@gmail.com>
2021-12-16 12:28:35 +0200

    Update Python solution for day 16

--- python/16/sol.py
+++ python/16/sol.py
@@ -3,91 +3,200 @@ from typing import *
 from heapq import *
 from functools import reduce
 from puzzle import PuzzleContext
+from abc import ABC, abstractclassmethod, abstractmethod
 
-class Packet:
-    type_id: str
-    id: int
-    version: int
+
+def _extract_header(s: str) -> Tuple[int, int]:
+    version = int(s[:3], 2)
+    type_id = int(s[3:6], 2)
+    return version, type_id
+
+
+class Packet(ABC):
+    def __init__(self, version: int):
+        self.version = version
+
+    @abstractclassmethod
+    def type_id(self) -> int:
+        pass
+
+    @abstractmethod
+    def evaluate(self) -> int:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def parse(cls, s: str) -> Optional[Tuple["Packet", str]]:
+        for subclass in cls.__subclasses__():
+            res = subclass.parse(s)
+            if res:
+                return res
+
+    def _render(self) -> str:
+        return f"Packet(type={self.type_id()}, version={self.version})"
+
+    def __str__(self):
+        return self._render()
+
+    def __repr__(self) -> str:
+        return str(self)
+
 
 class Literal(Packet):
-    value: int
+    def __init__(self, version: int, value: int):
+        super().__init__(version)
+        self.value = value
 
-    def __str__(self):
-        return f"Literal(type={self.type_id}, version={self.version}, value={self.value})"
+    @classmethod
+    def type_id(cls) -> int:
+        return 4
 
-    def __repr__(self) -> str:
-        return self.__str__()
+    @classmethod
+    def parse(cls, s: str) -> Optional[Tuple[Packet, str]]:
+        version, type_id = _extract_header(s)
+        if type_id != 4:
+            return None
+        i = 6
+        res = ""
+        while i < len(s):
+            flag = s[i]
+            res += s[i + 1 : i + 5]
+            i += 5
+            if flag == "0":
+                break
+        packet = cls(version, int(res, 2))
+        return packet, s[i:]
+
+    def evaluate(self) -> int:
+        return self.value
+
+    def _render(self) -> str:
+        return f"Literal(version={self.version}, value={self.value})"
+
 
 class Operator(Packet):
-    children: List[Packet]
+    def __init__(self, version: int, children: List[Packet]):
+        super().__init__(version)
+        self.children = children
+        self._name = self.__class__.__name__
 
-    def __str__(self):
-        return f"Operator(type_id={self.type_id}, version={self.version}, children={self.children})"
+    @classmethod
+    def parse(cls, s: str) -> Optional[Tuple[Packet, str]]:
+        version, type_id = _extract_header(s)
+        operator = cls._determine_operator_type(type_id)
+        if not operator:
+            return None
+        children, rest = cls._parse_children(s[6:])
+        return operator(version, children), rest
 
-    def __repr__(self) -> str:
-        return self.__str__()
+    @classmethod
+    def _determine_operator_type(cls, type_id: int) -> Optional[Type[Packet]]:
+        subclasses = [sub for sub in cls.__subclasses__() if sub.type_id() == type_id]
+        if len(subclasses) != 1:
+            return None
+        return subclasses[0]
 
-def parse_literal(s: str) -> Tuple[Packet, str]:
-    i = 0
-    res = ""
-    while i < len(s):
-        flag = s[i]
-        res += s[i+1:i+5]
-        i += 5
-        if flag == "0":
-            break
-    p = Literal()
-    p.value = int(res, 2)
-    return p, s[i:]
+    @classmethod
+    def _parse_children(cls, s: str) -> Tuple[List[Packet], str]:
+        length_type_id = s[0]
+        if length_type_id == "0":
+            return cls._parse_children_0(s[1:])
+        else:
+            return cls._parse_children_1(s[1:])
 
-def parse_operator_0(s: str) -> Tuple[Packet, str]:
-    l = int(s[:15], 2)
-    p = Operator()
-    p.children = parse(s[15:15+l])
-    return p, s[15+l:]
+    @classmethod
+    def _parse_children_0(cls, s: str) -> Tuple[List[Packet], str]:
+        subpackets_bit_length = int(s[:15], 2)
+        children_part = s[15 : 15 + subpackets_bit_length]
+        rest_part = s[15 + subpackets_bit_length :]
+        children = []
+        while len(children_part) > 0:
+            p, children_part = Packet.parse(children_part)
+            children.append(p)
+        return children, rest_part
+
+    @classmethod
+    def _parse_children_1(cls, s: str) -> Tuple[List[Packet], str]:
+        subpacket_count = int(s[:11], 2)
+        s = s[11:]
+        children = []
+        for _ in range(subpacket_count):
+            pp, s = Packet.parse(s)
+            children.append(pp)
+        return children, s
+
+    def _render(self) -> str:
+        return f"{self._name}(version={self.version}, children={self.children})"
 
 
-def parse_operator_1(s: str) -> Tuple[Packet, str]:
-    n_sub = int(s[:11], 2)
-    p = Operator()
-    p.children = []
-    s = s[11:]
-    for _ in range(n_sub):
-        pp, ss = parse_1(s)
-        p.children.append(pp)
-        s = ss
-    return p, s
-    
+class Sum(Operator):
+    @classmethod
+    def type_id(cls) -> int:
+        return 0
 
-def parse_operator(s: str) -> Packet:
-    l = s[0]
-    if l == "0":
-        return parse_operator_0(s[1:])
-    return parse_operator_1(s[1:])
+    def evaluate(self) -> int:
+        return sum(x.evaluate() for x in self.children)
 
-def parse_1(s: str) -> Tuple[Packet, str]:
-    v = s[:3]
-    t = s[3:6]
-    if t == "100":
-        p, ss = parse_literal(s[6:])
-        p.type_id = int(t, 2)
-        p.version = int(v, 2)
-        return p, ss
-    else:
-        p, ss = parse_operator(s[6:])
-        p.type_id = int(t, 2)
-        p.version = int(v, 2)
-        return p, ss
 
-def parse(s: str) -> List[Packet]:
-    res = []
-    while len(s) > 0:
-        if all(c == "0" for c in s):
-            break 
-        p, ss = parse_1(s)
-        s = ss
-        res.append(p)
-    return res
+class Product(Operator):
+    @classmethod
+    def type_id(cls) -> int:
+        return 1
+
+    def evaluate(self) -> int:
+        return reduce(lambda a, b: a * b, (x.evaluate() for x in self.children), 1)
+
+
+class Minimum(Operator):
+    @classmethod
+    def type_id(cls) -> int:
+        return 2
+
+    def evaluate(self) -> int:
+        return min(x.evaluate() for x in self.children)
+
+
+class Maximum(Operator):
+    @classmethod
+    def type_id(cls) -> int:
+        return 3
+
+    def evaluate(self) -> int:
+        return max(x.evaluate() for x in self.children)
+
+
+class GreaterThan(Operator):
+    @classmethod
+    def type_id(cls) -> int:
+        return 5
+
+    def evaluate(self) -> int:
+        v1 = self.children[0].evaluate()
+        v2 = self.children[1].evaluate()
+        return 1 if v1 > v2 else 0
+
+
+class LessThan(Operator):
+    @classmethod
+    def type_id(cls) -> int:
+        return 6
+
+    def evaluate(self) -> int:
+        v1 = self.children[0].evaluate()
+        v2 = self.children[1].evaluate()
+        return 1 if v1 < v2 else 0
+
+
+class EqualTo(Operator):
+    @classmethod
+    def type_id(cls) -> int:
+        return 7
+
+    def evaluate(self) -> int:
+        v1 = self.children[0].evaluate()
+        v2 = self.children[1].evaluate()
+        return 1 if v1 == v2 else 0
+
 
 def get_version_sum(p: Packet) -> int:
     res = p.version
@@ -95,38 +204,20 @@ def get_version_sum(p: Packet) -> int:
         res += sum(get_version_sum(x) for x in p.children)
     return res
 
-def evaluate(p: Packet) -> int:
-    if isinstance(p, Literal):
-        assert isinstance(p, Literal)
-        return p.value
-    assert isinstance(p, Operator)
-    if p.type_id == 0:
-        return sum(evaluate(x) for x in p.children)
-    if p.type_id == 1:
-        return reduce(lambda a, b: a*b, [evaluate(x) for x in p.children], 1)
-    if p.type_id == 2:
-        return min(evaluate(x) for x in p.children)
-    if p.type_id == 3:
-        return max(evaluate(x) for x in p.children)
-    if p.type_id == 5:
-        v1 = evaluate(p.children[0])
-        v2 = evaluate(p.children[1])
-        return 1 if v1 > v2 else 0
-    if p.type_id == 6:
-        v1 = evaluate(p.children[0])
-        v2 = evaluate(p.children[1])
-        return 1 if v1 < v2 else 0
-    if p.type_id == 7:
-        v1 = evaluate(p.children[0])
-        v2 = evaluate(p.children[1])
-        return 1 if v1 == v2 else 0
+
+def hex_char_to_bin(c: str) -> str:
+    return bin(int(c, 16))[2:].zfill(4)
+
+
+def hex_to_bin(s: str) -> str:
+    return "".join(map(hex_char_to_bin, s))
+
 
 with PuzzleContext(year=2021, day=16) as ctx:
-    s = "".join([bin(int(c, 16))[2:].zfill(4) for c in ctx.data])
-    
-    packets = parse(s)
-    assert len(packets) == 1
-    packet = packets[0]
-    
+    s = hex_to_bin(ctx.data)
+
+    packet, s = Packet.parse(s)
+    assert all(c == "0" for c in s)
+
     ctx.submit(1, get_version_sum(packet))
-    ctx.submit(2, evaluate(packet))
+    ctx.submit(2, packet.evaluate())
