5e9ee93ede65e439469ad46e55e231c2adb46370
Vytautas Strimaitis <vstrimaitis@gmail.com>
2021-12-19 11:45:16 +0200

    Speed up Python solution for day 19

--- python/19/sol.py
+++ python/19/sol.py
@@ -4,11 +4,14 @@ from heapq import *
 from itertools import permutations
 from puzzle import PuzzleContext
 
+Vec3 = Tuple[int, int, int]
 ScannerReading = Tuple[int, int, int]
 ScannerData = List[ScannerReading]
 
+
 def parse(line: str) -> ScannerReading:
     return tuple(map(int, line.split(",")))
+
 
 def transforms():
     for p in permutations("xyz"):
@@ -16,7 +19,8 @@ def transforms():
         for dx in [-1, 1]:
             for dy in [-1, 1]:
                 for dz in [-1, 1]:
-                    yield(p, dx, dy, dz)
+                    yield (p, dx, dy, dz)
+
 
 def do_transform(x, y, z, t):
     m = {
@@ -25,20 +29,23 @@ def do_transform(x, y, z, t):
         "yxz": (lambda y, x, z: (x, y, z)),
         "yzx": (lambda y, z, x: (x, y, z)),
         "zxy": (lambda z, x, y: (x, y, z)),
-        "zyx": (lambda z, y,x : (x, y, z)),
+        "zyx": (lambda z, y, x: (x, y, z)),
     }
     p, dx, dy, dz = t
     x, y, z = m[p](x, y, z)
-    x = dx*x
-    y = dy*y
-    z = dz*z
+    x = dx * x
+    y = dy * y
+    z = dz * z
     return x, y, z
 
-def sub(a: ScannerReading, b: ScannerReading) -> ScannerReading:
-    return b[0]-a[0], b[1]-a[1], b[2]-a[2]
 
-def readings_match(s1: ScannerData, s2: ScannerData) -> bool:
-    vecs1 = [s for s in s1[1:]]
+def sub(a: ScannerReading, b: ScannerReading) -> ScannerReading:
+    return b[0] - a[0], b[1] - a[1], b[2] - a[2]
+
+
+def calc_direction_vectors(s: ScannerData) -> List[Vec3]:
+    return list(sorted([sub(a, b) for a in s for b in s]))
+
 
 def detect_overlap(s1, s2):
     for (bx1, by1, bz1) in s1:
@@ -60,72 +67,70 @@ def detect_overlap(s1, s2):
                 if len(common) == 12:
                     return x2, y2, z2, t
     return None
-   
 
-with PuzzleContext(year=2021, day=19) as ctx:
-    scanners = []
-    for block in ctx.data.split("\n\n"):
-        scanners.append(list(map(parse, block.split("\n")[1:])))
-    positions = [None]*len(scanners)
-    positions[0] = (0, 0, 0, ("xyz", 1, 1, 1))
-    # print(detect_overlap(scanners[0], scanners[1])) # exp: (68, -1246, -43)
-    # print(detect_overlap(scanners[1], scanners[4])) # exp: (-88, 113, 1104)
 
-    pairs = [(i, j) for i in range(len(scanners)) for j in range(len(scanners)) if i != j]
-    while len(pairs) > 0:
-        for i, j in pairs:
-            if positions[i] is not None and positions[j] is not None:
-                pairs = [(ii, jj) for (ii, jj) in pairs if (ii, jj) != (i, j)]
-                break
-            if positions[i] is not None and positions[j] is None:
-                print("Trying:", i, j)
+if __name__ == "__main__":
+    with PuzzleContext(year=2021, day=19) as ctx:
+        scanners = []
+        for block in ctx.data.split("\n\n"):
+            scanners.append(list(map(parse, block.split("\n")[1:])))
+        positions = [None] * len(scanners)
+        positions[0] = (0, 0, 0, ("xyz", 1, 1, 1))
+
+        print("Preprocessing...")
+        direction_vectors = list(map(set, map(calc_direction_vectors, scanners)))
+        direction_vectors_with_transforms = [
+            [
+                set([do_transform(v[0], v[1], v[2], t) for v in d])
+                for t in transforms()
+            ]
+            for d in direction_vectors
+        ]
+        print("Finding pairs to connect...")
+        to_try = []
+        for i in range(len(scanners)):
+            for j in range(i+1, len(scanners)):
+                if i == j:
+                    continue
+                if any(len(direction_vectors[i] & d) > 100 for d in direction_vectors_with_transforms[j]):
+                    to_try.append((i, j))
+
+        print("Connecting...")
+        while len(to_try) > 0:
+            for i, j in to_try:
+                if positions[i] is not None and positions[j] is not None:
+                    to_try = [
+                        (ii, jj) for (ii, jj) in to_try if (ii, jj) != (i, j)
+                    ]
+                    break
+                if positions[i] is None and positions[j] is None:
+                    continue
+                if positions[i] is None and positions[j] is not None:
+                    i, j = j, i
                 s1 = scanners[i]
                 s2 = scanners[j]
                 p = detect_overlap(s1, s2)
                 if p is not None:
-                    # x1, y1, z1 = do_transform(*positions[i])
                     x1, y1, z1, _ = positions[i]
                     x2, y2, z2, t = p
-                    positions[j] = (x1+x2,y1+y2,z1+z2, t)
+                    positions[j] = (x1 + x2, y1 + y2, z1 + z2, t)
                     for k, s in enumerate(scanners[j]):
                         scanners[j][k] = do_transform(*scanners[j][k], t)
-                    print("Found!")
-                pairs = [(ii, jj) for (ii, jj) in pairs if (ii, jj) != (i, j)]
+                    print("Connected: ", i, j)
+                to_try = [(ii, jj) for (ii, jj) in to_try if (ii, jj) != (i, j)]
                 break
 
-    # while any(x is None for x in positions):
-    #     print("Trying...")
-    #     for i, s1 in enumerate(scanners):
-    #         print(i)
-    #         if positions[i] is None:
-    #             continue
-    #         for j, s2 in enumerate(scanners):
-    #             if positions[j] is not None:
-    #                 continue
-    #             p = detect_overlap(s1, s2)
-    #             if p is not None:
-    #                 # x1, y1, z1 = do_transform(*positions[i])
-    #                 x1, y1, z1, _ = positions[i]
-    #                 x2, y2, z2, t = p
-    #                 positions[j] = (x1+x2,y1+y2,z1+z2, t)
-    #                 for k, s in enumerate(scanners[j]):
-    #                     scanners[j][k] = do_transform(*scanners[j][k], t)
-    #                 # print("found: ", i, j, positions[j], x1, y1, z1)
-    # o = detect_overlap(scanners[0], scanners[1])
-    # print(o)
-    print(positions)
+        beacons = set()
+        for scanner, (x, y, z, t) in zip(scanners, positions):
+            for s in scanner:
+                sx, sy, sz = s
+                sx, sy, sz = sx + x, sy + y, sz + z
+                beacons.add((sx, sy, sz))
 
-    beacons = set()
-    for scanner, (x, y, z, t) in zip(scanners, positions):
-        for s in scanner:
-            sx, sy, sz = s
-            sx, sy, sz = sx+x, sy+y, sz+z
-            beacons.add((sx, sy, sz))
+        ans2 = 0
+        for x1, y1, z1, _ in positions:
+            for x2, y2, z2, _ in positions:
+                ans2 = max(ans2, abs(x1 - x2) + abs(y1 - y2) + abs(z1 - z2))
 
-    ans2 = 0
-    for x1,y1,z1, _ in positions:
-        for x2,y2,z2, _ in positions:
-            ans2 = max(ans2, abs(x1-x2)+abs(y1-y2)+abs(z1-z2))
-
-    ctx.submit(1, len(beacons))
-    ctx.submit(2, ans2)
+        ctx.submit(1, len(beacons))
+        ctx.submit(2, ans2)
